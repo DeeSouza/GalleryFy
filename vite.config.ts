184b63037a6620f5135a5b8bb211322a5c7da774
Diego Souza <dee-souza@hotmail.com>
2023-07-26 12:22:23 -0300

    chore: configure build and rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import typescript from "@rollup/plugin-typescript";
+
 import { fileURLToPath, URL } from "url";
+import path from "path";
 
 export default defineConfig({
   resolve: {
@@ -29,5 +32,33 @@ export default defineConfig({
       },
     ],
   },
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/main.js"),
+      name: "GalleryFy",
+      formats: ["es", "umd"],
+      fileName: (format) => `main.${format}.js`,
+    },
+
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [
+    typescript({
+      target: "es2022",
+      rootDir: "./src",
+      declaration: true,
+      declarationDir: "./dist",
+      exclude: "./node_modules/**",
+      allowSyntheticDefaultImports: true,
+    }),
+    react(),
+  ],
 });
