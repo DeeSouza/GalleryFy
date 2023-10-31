3a45ffca46957c6e1ff7c5fefad4c3b5f44707b2
Diego Souza <dee-souza@hotmail.com>
2023-10-31 12:38:30 -0300

    Updates

---
+++ components/ButtonClose/index.d.ts
@@ -0,0 +1,2 @@
+import { ButtonCloseProps } from "./types";
+export declare function ButtonClose({ handleClose }: ButtonCloseProps): import("react/jsx-runtime").JSX.Element;
--- components/GalleryFy/index.d.ts
+++ components/GalleryFy/index.d.ts
@@ -7,6 +7,7 @@ import { GalleryFyProps } from "./types";
  * @param open control open/close gallery
  * @param handleClose function to close gallery
  * @param showThumbs show thumbs in the gallery
+ * @param positionPlacement position of the control buttons
  * @returns
  */
 declare const GalleryFy: React.FunctionComponent<GalleryFyProps>;
